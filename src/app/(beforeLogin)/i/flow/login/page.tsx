d9a62d21799693f6f68ae0690e709fdd4016e409
kjw-blog <rkdwjddnr11@naver.com>
2024-01-19 16:52:08 +0900

    feat: 패러렐 라우트 적용

--- src/app/(beforeLogin)/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main></main>;
+  return <main>로그인 모달</main>;
 }
--- src/app/(beforeLogin)/i/flow/signup/page.tsx
+++ src/app/(beforeLogin)/i/flow/signup/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main></main>;
+  return <main>회원가입 모달</main>;
 }
