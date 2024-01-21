1c1a57ce823623dfd990bdfa78c3e59c6a0ab134
kjw-blog <rkdwjddnr11@naver.com>
2024-01-21 22:51:08 +0900

    feat: home 페이지 퍼블리싱
    - 탭 부분 퍼블리싱
    - home 페이지 Context API 적용

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,28 @@
+import style from './home.module.css';
+
+import Tab from '@/app/(afterLogin)/home/_component/Tab';
+import PostForm from '@/app/(afterLogin)/home/_component/PostForm';
+import Post from '@/app/(afterLogin)/_component/Post';
+import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider';
+
 export default function Home() {
-  return <main>홈 페이지</main>;
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        {/* <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post /> */}
+      </TabProvider>
+    </main>
+  );
 }
