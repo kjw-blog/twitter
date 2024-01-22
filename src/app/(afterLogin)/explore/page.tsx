35b667aa8c3502f1c73009661d31d7d2c929504c
kjw-blog <rkdwjddnr11@naver.com>
2024-01-22 17:10:39 +0900

    feat: explore 페이지 퍼블리싱
    - explore 페이지 퍼블리싱
    - pathname에 따라 UI 변경

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,24 @@
+import style from './explore.module.css';
+import SearchForm from '@/app/(afterLogin)/_component/SearchForm';
+import Trend from '../_component/Trend';
+
 export default function Home() {
-  return <main>검색 페이지</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
