9fccce1702b985eb8ef101c7961691a9103dfa7f
kjw-blog <rkdwjddnr11@naver.com>
2024-01-27 23:19:43 +0900

    feat: react-query 세팅
    - defaultOptions 세팅
    - 메인 페이지 react-query 세팅

--- src/app/(afterLogin)/_component/RQProvider.tsx
+++ src/app/(afterLogin)/_component/RQProvider.tsx
@@ -12,7 +12,12 @@ function RQProvider({ children }: Props) {
   const [client] = useState(
     new QueryClient({
       defaultOptions: {
-        queries: { refetchOnWindowFocus: false, retry: false },
+        queries: {
+          refetchOnWindowFocus: false, // 탭 전환 후 돌아올 경우 refetch 옵션
+          retry: false, // fetching 실패 시 재시도 number | false
+          refetchOnMount: true, // 컴포넌트가 마운트될 때 refetch 옵션
+          refetchOnReconnect: false, // 인터넷 연결이 끊겼다가 다시 연결이 될 때 refetch 옵션
+        },
       },
     })
   );
--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -9,6 +9,9 @@ export default function PostRecommends() {
   const { data } = useQuery<IPost[]>({
     queryKey: ['posts', 'recommends'],
     queryFn: getPostRecommends,
+    // staleTime은 항상 gcTime보다 짧아야함
+    staleTime: 60 * 1000, // 기본값 0, 데이터가 fresh에서 stale로 변경되는 시간 ms
+    gcTime: 300 * 1000, // 기본값 300000ms(5분) inactive 상태의 사용하지 않는 데이터를 메모리에서 정리를 해주는 시간
   });
 
   return data?.map((post) => <Post key={post.postId} post={post} />);
