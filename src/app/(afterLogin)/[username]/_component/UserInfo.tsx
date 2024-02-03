344a87e0006137835d0cec0768c68ce3a11679c7
kjw-blog <rkdwjddnr11@naver.com>
2024-02-03 21:29:23 +0900

    style: 유저 정보에 팔로우/팔로잉 UI 추가

--- src/app/(afterLogin)/[username]/_component/UserInfo.tsx
+++ src/app/(afterLogin)/[username]/_component/UserInfo.tsx
@@ -284,21 +284,27 @@ export default function UserInfo({ username }: Props) {
         <h3 className={style.headerTitle}>{user.nickname}</h3>
       </div>
       <div className={style.userZone}>
-        <div className={style.userImage}>
-          <img src={user.image} alt={user.id} />
+        <div className={style.userRow}>
+          <div className={style.userImage}>
+            <img src={user.image} alt={user.id} />
+          </div>
+          <div className={style.userName}>
+            <div>{user.nickname}</div>
+            <div>@{user.id}</div>
+          </div>
+          {user.id !== session?.user?.email && (
+            <button
+              onClick={onFollow}
+              className={cx(style.followButton, followed && style.followed)}
+            >
+              {followed ? '팔로잉' : '팔로우'}
+            </button>
+          )}
         </div>
-        <div className={style.userName}>
-          <div>{user.nickname}</div>
-          <div>@{user.id}</div>
+        <div className={style.userFollower}>
+          <div>{user._count.Followers} 팔로워</div>&nbsp;
+          <div>{user._count.Followings} 팔로우 중</div>
         </div>
-        {user.id !== session?.user?.email && (
-          <button
-            onClick={onFollow}
-            className={cx(style.followButton, followed && style.followed)}
-          >
-            {followed ? '팔로잉' : '팔로우'}
-          </button>
-        )}
       </div>
     </>
   );
