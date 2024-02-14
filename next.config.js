f69ed0570c981ef6e108c742c1b4c7e3c8377719
kjw-blog <rkdwjddnr11@naver.com>
2024-02-14 22:48:59 +0900

    memo: static 모드 배포에 대한 메모

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // output: 'export',
   async rewrites() {
     return [
       {
@@ -11,3 +12,15 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+/**
+ * output: 'export'
+ *
+ * next의 2가지 배포 모드 중 하나인 static모드로 설정하는 코드
+ * 설정을 하지 않으면 dynamic 모드임
+ *
+ * static모드 : next 서버 없이 HTML 페이지들로만 구성된 정적인 사이트
+ *              build시 모든 것이 결정되기 때문에 서버가 필요없음
+ *
+ * 그 외 거의 모든 프로젝트는 dynamic 모드로 해야한다.
+ */
