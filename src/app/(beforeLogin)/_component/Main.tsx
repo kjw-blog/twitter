341503f62d1ba12eee3d538dbac7db6bca116f1d
kjw-blog <rkdwjddnr11@naver.com>
2024-02-22 22:19:45 +0900

    style: 로그인 화면 vanila extract로 변경

--- src/app/(beforeLogin)/_component/Main.tsx
+++ src/app/(beforeLogin)/_component/Main.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import zLogo from '../../../../public/zlogo.png';
-import styles from '@/app/(beforeLogin)/_component/main.module.css';
+import * as styles from '@/app/(beforeLogin)/_component/main.css';
 
 export default function Main() {
   return (
